23a81c174133029ad96220616353bb0acd05fb58
Madeline THOMAN <mthoman@e2r8p18.42.fr>
2018-11-14 18:35:02 +0100

    deletes

--- libft/main.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-#include <stdio.h>
-
-int	main(void)
-{
-  printf("%s%s\n", "memchr: ", memchr("abcdef", 999, 6));
-  printf("%s%s\n", "ft_memchr: ", ft_memchr("abcdef", 999, 6));
-  return (0);
-}
